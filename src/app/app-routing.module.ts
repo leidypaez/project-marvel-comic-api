d465deba935024cab832d54db50013ca29b879af
leidypaez <leidy16paez@gmail.com>
2023-05-26 22:27:21 -0500

    Create components for each page and images for header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './componets/landing-page/landing-page.component';
+import { HomeComponent } from './componets/home/home.component';
+import { HeaderComponent } from './componets/header/header.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: "", component:LandingPageComponent},
+  {path: "home", component:HomeComponent},
+  {path: "header", component:HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './componets/landing-page/landing-page.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { HomeComponent } from './componets/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
